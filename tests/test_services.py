f05916ff16f1d4659e9e81d30953d908bc8ae5f1
danil <danil369.ru@mail.ru>
2023-03-08 19:11:32 +0600

    test

--- tests/test_services.py
+++ tests/test_services.py
@@ -5,31 +5,31 @@ from service2 import service2
 from service3 import service3
 
 
-# def test_service1(conn):
-#     transaction = service1(conn)
-#     transactions = get_transactions(conn)
-#     assert len(transactions) == 1
-#     assert transactions[0].description == transaction.description
-#
-#
-# def test_service2(conn):
-#     for _ in range(5):
-#         service1(conn)
-#
-#     service2(conn)
-#     transactions = get_transactions(conn)
-#     for transaction in transactions:
-#         assert transaction.check_status == "sending"
-#         assert transaction.price * transaction.amount == transaction.quantity
-#
-#
-# def test_service3(conn):
-#     test_service2(conn)
-#     service3(conn)
-#     transactions = get_transactions(conn)
-#     for transaction in transactions:
-#         assert transaction.check_status == "sent"
-#
+def test_service1(conn):
+    transaction = service1(conn)
+    transactions = get_transactions(conn)
+    assert len(transactions) == 1
+    assert transactions[0].description == transaction.description
+
+
+def test_service2(conn):
+    for _ in range(5):
+        service1(conn)
+
+    service2(conn)
+    transactions = get_transactions(conn)
+    for transaction in transactions:
+        assert transaction.check_status == "sending"
+        assert transaction.price * transaction.amount == transaction.quantity
+
+
+def test_service3(conn):
+    test_service2(conn)
+    service3(conn)
+    transactions = get_transactions(conn)
+    for transaction in transactions:
+        assert transaction.check_status == "sent"
+
 
 def test_show_error():
-    assert 1 == 0
+    assert 1 != 0
